6a5bdf8959b72de27920e39968dd19a9573c8f0e
Liliia Sviliak <liliia.belei@gmail.com>
2021-11-06 19:46:04 +0200

    hw6-mail

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -2,10 +2,12 @@ const signup = require("./signup");
 const login = require("./login");
 const logout = require("./logout");
 const avatarUpdate = require("./avatarUpdate");
+const verify = require('./verify');
 
 module.exports = {
     signup,
     login ,
     logout,
-    avatarUpdate
+    avatarUpdate,
+    verify
 }
